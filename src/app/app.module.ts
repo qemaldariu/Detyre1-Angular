044d77cf67896e33e468cfde3c42505ae99bee91
qemaldariu <128551042+qemaldariu@users.noreply.github.com>
2024-03-13 23:32:21 +0100

    second excercise done,used bootstrapp classes for the view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CounterComponentComponent } from './counter-component/counter-component.component';
 import { InputComponentComponent } from './input-component/input-component.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { InputComponentComponent } from './input-component/input-component.compo
     InputComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/input-component/input-component.component.ts
+++ src/app/input-component/input-component.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./input-component.component.css']
 })
 export class InputComponentComponent {
+  inputValue!:string;
+  imgUrl:string="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png"
 
+resetInput(event:Event){
+   this.inputValue='';
+
+
+}
 }
